e8c31aa6eea6d4be4794b25fcf9b0b7275af66e6
Stephen Dolan <mu@netsoc.tcd.ie>
2014-05-12 11:24:54 +0100

    Squash more globals by moving all startup parameters to startup.c

--- byterun/instrtrace.c
+++ byterun/instrtrace.c
@@ -26,14 +26,13 @@
 #include "prims.h"
 #include "stacks.h"
 #include "domain.h"
+#include "startup.h"
 
 extern code_t caml_start_code;
 
 __thread intnat caml_icount = 0;
 
 void caml_stop_here () {}
-
-int caml_trace_flag = 0;
 
 char * caml_instr_string (code_t pc);
 
@@ -165,7 +164,7 @@ caml_trace_accu_sp_file (value accu, value * sp, code_t prog, int proglen,
   caml_trace_value_file (accu, prog, proglen, f);
   fprintf (f, "\n sp=%#" ARCH_INTNAT_PRINTF_FORMAT "x @%ld:",
            (intnat) sp, caml_stack_high - sp);
-  for (p = sp, i = 0; i < 12 + (1 << caml_trace_flag) && p < caml_stack_high;
+  for (p = sp, i = 0; i < 12 + (1 << caml_startup_params.trace_flag) && p < caml_stack_high;
        p++, i++) {
     fprintf (f, "\n[%ld] ", caml_stack_high - p);
     caml_trace_value_file (*p, prog, proglen, f);
