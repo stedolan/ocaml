494d2e9a67c4bfa653de3c967273742902d6d74a
Sadiq <sadiq@toao.com>
2020-09-11 15:18:10 +0100

    work breaking liveness

--- asmcomp/polling.ml
+++ asmcomp/polling.ml
@@ -6,6 +6,19 @@ end
 
 module IntSet = Set.Make (IntArg)
 
+let add_iteration_counter_before (f : Mach.instruction) : Mach.instruction =
+  let new_live = Reg.Set.union f.live (Reg.Set.of_seq (Array.to_seq f.arg)) in
+  {
+    desc = Iop Ipoll;
+    next = f;
+    arg = Array.make 0 Reg.dummy;
+    res = Array.make 0 Reg.dummy;
+    dbg = f.dbg;
+    live = new_live;
+    available_before = f.available_before;
+    available_across = f.available_across;
+  }
+
 let add_poll_before (f : Mach.instruction) : Mach.instruction =
   let new_live = Reg.Set.union f.live (Reg.Set.of_seq (Array.to_seq f.arg)) in
   {
@@ -174,57 +187,52 @@ let rec find_rec_handlers (f : Mach.instruction) =
       IntSet.empty
   | Iop _ -> find_rec_handlers f.next
 
-let rec add_polls_to_exit (forward_handlers : int list)
-    (rec_handlers : IntSet.t) (f : Mach.instruction) =
-  let forward_rec_add_polls = add_polls_to_exit forward_handlers rec_handlers in
+let instrument_loops_with_polls (rec_handlers : IntSet.t) (i : Mach.instruction) =
+  let rec instrument_loops (f : Mach.instruction) =
   match f.desc with
   | Iifthenelse (test, i0, i1) ->
       {
         f with
         desc =
-          Iifthenelse (test, forward_rec_add_polls i0, forward_rec_add_polls i1);
-        next = forward_rec_add_polls f.next;
+          Iifthenelse (test, instrument_loops i0, instrument_loops i1);
+        next = instrument_loops f.next;
       }
   | Iswitch (index, cases) ->
       {
         f with
-        desc = Iswitch (index, Array.map forward_rec_add_polls cases);
-        next = forward_rec_add_polls f.next;
+        desc = Iswitch (index, Array.map instrument_loops cases);
+        next = instrument_loops f.next;
       }
   | Icatch (rec_flag, handlers, body) ->
-      {
+      let new_f = {
         f with
         desc =
           Icatch
             ( rec_flag,
               List.map
-                (fun (idx, instrs) -> (idx, forward_rec_add_polls instrs))
+                (fun (idx, instrs) -> (idx, instrument_loops instrs))
                 handlers,
-              add_polls_to_exit
-                (forward_handlers @ List.map (fun (i, _) -> i) handlers)
-                rec_handlers body );
-        next = forward_rec_add_polls f.next;
-      }
+              instrument_loops body );
+        next = instrument_loops f.next;
+      } in
+        add_iteration_counter_before new_f
   | Itrywith (body, handler) ->
       {
         f with
         desc =
-          Itrywith (forward_rec_add_polls body, forward_rec_add_polls handler);
-        next = forward_rec_add_polls f.next;
+          Itrywith (instrument_loops body, instrument_loops handler);
+        next = instrument_loops f.next;
       }
   | Iexit id ->
-      if
-        IntSet.mem id rec_handlers
-        && not (List.exists (fun a -> a == id) forward_handlers)
-      then
-        let new_f = add_poll_before f in
-        { new_f with next = { f with next = forward_rec_add_polls f.next } }
-      else { f with Mach.next = forward_rec_add_polls f.next }
+        let new_f = if IntSet.mem id rec_handlers then add_poll_before f else f 
+        in
+        { new_f with next = { f with next = instrument_loops f.next } }
   | Iend | Ireturn | Iop (Itailcall_ind _) | Iop (Itailcall_imm _) | Iraise _ ->
       f
-  | Iop _ -> { f with next = forward_rec_add_polls f.next }
+  | Iop _ -> { f with next = instrument_loops f.next }
+  in instrument_loops i
 
 let funcdecl (i : Mach.fundecl) : Mach.fundecl =
   let f = i.fun_body in
   let rec_handlers = find_rec_handlers f in
-  { i with fun_body = add_polls_to_exit [] rec_handlers f }
+  { i with fun_body = instrument_loops_with_polls rec_handlers f }
