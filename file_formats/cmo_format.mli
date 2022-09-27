9b1662bb9a4167a5c8ce67ebe68ef1a9e3b6a2ad
Stephen Dolan <sdolan@janestreet.com>
2022-09-27 18:31:38 +0100

    WIP cmo_format

--- file_formats/cmo_format.mli
+++ file_formats/cmo_format.mli
@@ -15,15 +15,18 @@
 
 (* Symbol table information for .cmo and .cma files *)
 
-open Misc
+type modname = string
+type globalname = string
+type crcs = (modname * Digest.t option) list
 
 (* Relocation information *)
 
 type reloc_info =
-    Reloc_literal of Lambda.structured_constant    (* structured constant *)
-  | Reloc_getglobal of Ident.t              (* reference to a global *)
-  | Reloc_setglobal of Ident.t              (* definition of a global *)
-  | Reloc_primitive of string               (* C primitive number *)
+    Reloc_literal of Obj.t        (* translated structured constant *)
+  | Reloc_getpredef of string     (* reference to a predefined value *)
+  | Reloc_getglobal of globalname (* reference to a global *)
+  | Reloc_setglobal of globalname (* definition of a global *)
+  | Reloc_primitive of string     (* C primitive number *)
 
 (* Descriptor for compilation units *)
 
@@ -33,7 +36,7 @@ type compilation_unit =
     cu_codesize: int;                   (* Size of code block *)
     cu_reloc: (reloc_info * int) list;  (* Relocation information *)
     cu_imports: crcs;                   (* Names and CRC of intfs imported *)
-    cu_required_globals: Ident.t list;  (* Compilation units whose
+    cu_required_globals: globalname list;  (* Compilation units whose
                                            initialization side effects
                                            must occur before this one. *)
     cu_primitives: string list;         (* Primitives declared inside *)
