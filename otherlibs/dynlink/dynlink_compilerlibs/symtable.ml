710de156cf2f7423d522032e45dcbebefa64dadb
Stephen Dolan <sdolan@janestreet.com>
2023-09-27 11:44:24 +0100

    dynlink_compilerlibs: Remove some unused parts of symtable.ml

--- otherlibs/dynlink/dynlink_compilerlibs/symtable.ml
+++ otherlibs/dynlink/dynlink_compilerlibs/symtable.ml
@@ -16,7 +16,6 @@
 (* To assign numbers to globals and primitives *)
 
 open Misc
-open Lambda
 open Cmo_format
 
 module String = Misc.Stdlib.String
@@ -26,17 +25,6 @@ module Compunit = struct
   type t = compunit
   let name (Compunit cu_name) = cu_name
   let is_packed (Compunit name) = String.contains name '.'
-  let to_ident (Compunit cu_name) = Ident.create_persistent cu_name
-  module Set = Set.Make(struct type nonrec t = t let compare = compare end)
-  module Map = Map.Make(struct type nonrec t = t let compare = compare end)
-end
-
-let builtin_values = Predef.builtin_values
-
-module Predef = struct
-  type t = predef
-  module Set = Set.Make(struct type nonrec t = t let compare = compare end)
-  module Map = Map.Make(struct type nonrec t = t let compare = compare end)
 end
 
 module Global = struct
@@ -57,15 +45,6 @@ module Global = struct
         Format.fprintf ppf "predefined exception %a"
           Style.inline_code (quote exn)
 
-  let of_ident id =
-    let name = Ident.name id in
-    if (Ident.is_predef id)
-    then Some (Glob_predef (Predef_exn name))
-    else if (Ident.global id)
-    then Some (Glob_compunit (Compunit name))
-    else None
-
-  module Set = Set.Make(struct type nonrec t = t let compare = compare end)
   module Map = Map.Make(struct type nonrec t = t let compare = compare end)
 end
 
@@ -110,9 +89,6 @@ module PrimMap = Num_tbl(Misc.Stdlib.String.Map)
 let global_table = ref GlobalMap.empty
 and literal_table = ref([] : (int * Obj.t) list)
 
-let is_global_defined global =
-  Global.Map.mem global (!global_table).tbl
-
 let slot_for_getglobal global =
   try
     GlobalMap.find !global_table global
@@ -153,116 +129,6 @@ let of_prim name =
           num
     end
 
-let require_primitive name =
-  if name.[0] <> '%' then ignore(of_prim name)
-
-let all_primitives () =
-  let prim = Array.make !c_prim_table.cnt "" in
-  String.Map.iter (fun name number -> prim.(number) <- name) !c_prim_table.tbl;
-  prim
-
-let data_primitive_names () =
-  all_primitives()
-  |> Array.to_list
-
-let output_primitive_names outchan =
-  output_string outchan (data_primitive_names() |> concat_null_terminated)
-
-open Printf
-
-let output_primitive_table outchan =
-  let prim = all_primitives() in
-  for i = 0 to Array.length prim - 1 do
-    fprintf outchan "extern value %s(void);\n" prim.(i)
-  done;
-  fprintf outchan "typedef value (*c_primitive)(void);\n";
-  fprintf outchan "const c_primitive caml_builtin_cprim[] = {\n";
-  for i = 0 to Array.length prim - 1 do
-    fprintf outchan "  %s,\n" prim.(i)
-  done;
-  fprintf outchan "  0 };\n";
-  fprintf outchan "const char * const caml_names_of_builtin_cprim[] = {\n";
-  for i = 0 to Array.length prim - 1 do
-    fprintf outchan "  \"%s\",\n" prim.(i)
-  done;
-  fprintf outchan "  0 };\n"
-
-(* Translate structured constants *)
-
-let rec transl_const = function
-    Const_base(Const_int i) -> Obj.repr i
-  | Const_base(Const_char c) -> Obj.repr c
-  | Const_base(Const_string (s, _, _)) -> Obj.repr s
-  | Const_base(Const_float f) -> Obj.repr (float_of_string f)
-  | Const_base(Const_int32 i) -> Obj.repr i
-  | Const_base(Const_int64 i) -> Obj.repr i
-  | Const_base(Const_nativeint i) -> Obj.repr i
-  | Const_immstring s -> Obj.repr s
-  | Const_block(tag, fields) ->
-      let block = Obj.new_block tag (List.length fields) in
-      let transl_field pos cst =
-        Obj.set_field block pos (transl_const cst)
-      in
-      List.iteri transl_field fields;
-      block
-  | Const_float_array fields ->
-      let res = Array.Floatarray.create (List.length fields) in
-      List.iteri (fun i f -> Array.Floatarray.set res i (float_of_string f))
-        fields;
-      Obj.repr res
-
-(* Initialization for batch linking *)
-
-let init () =
-  (* Enter the predefined exceptions *)
-  Array.iteri
-    (fun i name ->
-        if not (List.mem_assoc name builtin_values)
-        then fatal_error "Symtable.init";
-      let global = Global.Glob_predef (Predef_exn name) in
-      let c = slot_for_setglobal global in
-      let cst = Const_block
-          (Obj.object_tag,
-           [Const_base(Const_string (name, Location.none,None));
-            Const_base(Const_int (-i-1))
-           ])
-      in
-      literal_table := (c, transl_const cst) :: !literal_table)
-    Runtimedef.builtin_exceptions;
-  (* Initialize the known C primitives *)
-  let set_prim_table_from_file primfile =
-    let ic = open_in primfile in
-    Misc.try_finally
-      ~always:(fun () -> close_in ic)
-      (fun () ->
-         try
-           while true do
-             set_prim_table (input_line ic)
-           done
-         with End_of_file -> ()
-      )
-  in
-  if String.length !Clflags.use_prims > 0 then
-    set_prim_table_from_file !Clflags.use_prims
-  else if String.length !Clflags.use_runtime > 0 then begin
-    let primfile = Filename.temp_file "camlprims" "" in
-    Misc.try_finally
-      ~always:(fun () -> remove_file primfile)
-      (fun () ->
-         let cmd =
-           Filename.quote_command
-             !Clflags.use_runtime
-             ~stdout:primfile
-             ["-p"]
-         in
-         if Sys.command cmd <> 0
-         then raise(Error(Wrong_vm !Clflags.use_runtime));
-         set_prim_table_from_file primfile
-      )
-  end else begin
-    Array.iter set_prim_table Runtimedef.builtin_primitives
-  end
-
 (* Relocate a block of object bytecode *)
 
 let patch_int buff pos n =
@@ -289,24 +155,6 @@ let patch_object buff patchlist =
       | (Reloc_primitive name, pos) ->
           patch_int buff pos (of_prim name))
     patchlist
-
-(* Build the initial table of globals *)
-
-let initial_global_table () =
-  let glob = Array.make !global_table.cnt (Obj.repr 0) in
-  List.iter
-    (fun (slot, cst) -> glob.(slot) <- cst)
-    !literal_table;
-  literal_table := [];
-  glob
-
-(* Save the table of globals *)
-
-let output_global_map oc =
-  output_value oc !global_table
-
-let data_global_map () =
-  Obj.repr !global_table
 
 (* Functions for toplevel use *)
 
@@ -342,12 +190,8 @@ let init_toplevel () =
 
 (* Find the value of a global identifier *)
 
-let get_global_position = slot_for_getglobal
-
 let get_global_value global =
   (Meta.global_data()).(slot_for_getglobal global)
-let assign_global_value global v =
-  (Meta.global_data()).(slot_for_getglobal global) <- v
 
 (* Check that all compilation units referenced in the given patch list
    have already been initialized *)
@@ -390,8 +234,6 @@ type global_map = GlobalMap.t
 
 let current_state () = !global_table
 
-let restore_state st = global_table := st
-
 let hide_additions (st : global_map) =
   if st.cnt > !global_table.cnt then
     fatal_error "Symtable.hide_additions";
@@ -399,20 +241,6 @@ let hide_additions (st : global_map) =
     {GlobalMap.
       cnt = !global_table.cnt;
       tbl = st.tbl }
-
-(* "Filter" the global map according to some predicate.
-   Used to expunge the global map for the toplevel. *)
-
-let filter_global_map p (gmap : global_map) =
-  let newtbl = ref Global.Map.empty in
-  Global.Map.iter
-    (fun global num ->
-      if p global then newtbl := Global.Map.add global num !newtbl)
-    gmap.tbl;
-  {GlobalMap. cnt = gmap.cnt; tbl = !newtbl}
-
-let iter_global_map f (gmap : global_map) =
-  Global.Map.iter f gmap.tbl
 
 let is_defined_in_global_map (gmap : global_map) global =
   Global.Map.mem global gmap.tbl
@@ -442,8 +270,3 @@ let () =
       | Error err -> Some (Location.error_of_printer_file report_error err)
       | _ -> None
     )
-
-let reset () =
-  global_table := GlobalMap.empty;
-  literal_table := [];
-  c_prim_table := PrimMap.empty
